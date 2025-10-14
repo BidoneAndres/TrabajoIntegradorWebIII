f598662b4d6b92a261331d04975cecb53d1b128c
BidoneAndres <bidoneandres@gmail.com>
2025-10-14 14:31:15 -0300

    Proyecto integrador

---
+++ trabajointegrador/src/test/java/ar/iua/edu/trabajointegrador/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.iua.edu.trabajointegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
