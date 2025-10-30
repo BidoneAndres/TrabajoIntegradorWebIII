baa515e24be900f5e63a4ee5c6670f5567da0f8d
EduardoFriesen <edufriesen2002@gmail.com>
2025-10-30 12:58:21 -0300

    asd

--- trabajointegrador/src/main/java/ar/iua/edu/trabajointegrador/model/business/interfaces/IProductoBusiness.java
+++ trabajointegrador/src/main/java/ar/iua/edu/trabajointegrador/model/business/interfaces/IProductoBusiness.java
@@ -4,10 +4,7 @@ import java.util.List;
 
 import ar.iua.edu.trabajointegrador.model.Producto;
 import ar.iua.edu.trabajointegrador.model.business.exceptions.BusinessException;
-<<<<<<< HEAD
 import ar.iua.edu.trabajointegrador.model.business.exceptions.FoundException;
-=======
->>>>>>> 0b9625395f4782de8dd9587c852b054b81219d9f
 import ar.iua.edu.trabajointegrador.model.business.exceptions.NotFoundException;
 
 public interface IProductoBusiness {
@@ -16,9 +13,6 @@ public interface IProductoBusiness {
 	public Producto load(long id) throws NotFoundException, BusinessException;
 	
 	public Producto load(String producto) throws NotFoundException, BusinessException;
-<<<<<<< HEAD
 	
 	public Producto addProducto(Producto producto) throws NotFoundException, BusinessException, FoundException;
-=======
->>>>>>> 0b9625395f4782de8dd9587c852b054b81219d9f
 }
