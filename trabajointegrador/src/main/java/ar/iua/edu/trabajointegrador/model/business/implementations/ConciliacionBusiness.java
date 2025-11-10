e22b9adf97fad8e044f1d0e455b8c5001a9b7dbb
joaquincernik <joaquincernik@gmail.com>
2025-11-10 11:08:24 -0300

    agregado de metodos en conciliacion

--- trabajointegrador/src/main/java/ar/iua/edu/trabajointegrador/model/business/implementations/ConciliacionBusiness.java
+++ trabajointegrador/src/main/java/ar/iua/edu/trabajointegrador/model/business/implementations/ConciliacionBusiness.java
@@ -1,5 +1,6 @@
 package ar.iua.edu.trabajointegrador.model.business.implementations;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,42 +19,42 @@ import ar.iua.edu.trabajointegrador.model.business.interfaces.IOrdenBusiness;
 import ar.iua.edu.trabajointegrador.model.persistence.ConciliacionRepository;
 import lombok.extern.slf4j.Slf4j;
 
-
-
 @Service
 @Slf4j
 public class ConciliacionBusiness implements IConciliacionBusiness {
 
 	@Autowired
 	private IOrdenBusiness ordenBusiness;
-	
+
 	@Autowired
 	private IDatoCargaBusiness datoCargaBusiness;
-	
+
 	@Autowired
 	private IDatoCargaHeaderBusiness datoCargaHeaderBusiness;
-	
-	
+
 	@Autowired
 	private ConciliacionRepository concilacionDAO;
-	
 
 	@Override
-	public Conciliacion add(float pesajeFinal, int numeroOrden) throws NotFoundException, BusinessException, StateLoadException {
-		//busqueda de la orden
+	public Conciliacion add(float pesajeFinal, int numeroOrden)
+			throws NotFoundException, BusinessException, StateLoadException {
+		// busqueda de la orden
 		Optional<Orden> orden = ordenBusiness.findByNumeroOrden(numeroOrden);
-		Double productoCargado = datoCargaHeaderBusiness.findByOrdenId(orden.get().getId()).get().getUltimaMasaAcumulada();
-		if(productoCargado == null || orden.isEmpty() ) {
-			throw NotFoundException.builder().message("No se encontro la orden asociada a esa clave de activacion").build();			
-		}
-		else {
-			if(orden.get().getEstado() != Estado.ESTADO_3_CERRADA_PARA_CARGA) {
-				throw StateLoadException.builder().message("La orden no esta en ESTADO_3_CERRADA_PARA_CARGA, estado actual: " + orden.get().getEstado()).build();	
-				}
-			//guardamos en la orden el pesaje final
+		Double productoCargado = datoCargaHeaderBusiness.findByOrdenId(orden.get().getId()).get()
+				.getUltimaMasaAcumulada();
+		if (productoCargado == null || orden.isEmpty()) {
+			throw NotFoundException.builder().message("No se encontro la orden asociada a esa clave de activacion")
+					.build();
+		} else {
+			if (orden.get().getEstado() != Estado.ESTADO_3_CERRADA_PARA_CARGA) {
+				throw StateLoadException.builder().message(
+						"La orden no esta en ESTADO_3_CERRADA_PARA_CARGA, estado actual: " + orden.get().getEstado())
+						.build();
+			}
+			// guardamos en la orden el pesaje final
 			orden.get().setEstado(Orden.Estado.ESTADO_4_FINALIZADA);
 			orden.get().setPesoFinal(pesajeFinal);
-			
+
 			Conciliacion conciliacion = new Conciliacion();
 			conciliacion.setOrden(orden.get());
 			conciliacion.setPesoFinal(pesajeFinal);
@@ -62,18 +63,44 @@ public class ConciliacionBusiness implements IConciliacionBusiness {
 			float neto = pesajeFinal - orden.get().getPesoInicial();
 			conciliacion.setNetoPorBalanza(neto);
 			conciliacion.setDiferenciaBalanzaCaudalimetro(neto - productoCargado);
-			
-			//promedios
+
+			// promedios
 			int numeroOrden1 = orden.get().getNumeroOrden();
 			conciliacion.setPromedioDensidad(datoCargaBusiness.calculateDensidadProductoAvg(numeroOrden1).get());
 			conciliacion.setPromedioCaudal(datoCargaBusiness.calculateCaudalAvg(numeroOrden1).get());
 			conciliacion.setPromedioTemperatura(datoCargaBusiness.calculateTemperaturaAvg(numeroOrden1).get());
-			
+
 			return concilacionDAO.save(conciliacion);
-			
-			
-			
+
 		}
+	}
+
+	@Override
+	public List<Conciliacion> list() throws BusinessException {
+		try {
+			return concilacionDAO.findAll();
+		} catch (Exception e) {
+			throw BusinessException.builder().message(e.getMessage()).build();
+		}
+	}
+
+	@Override
+	public Conciliacion loadByNumeroOrden(int numeroOrden) throws BusinessException, NotFoundException {
+		try {
+			Optional<Conciliacion> c = concilacionDAO.findByNumeroOrden(numeroOrden);
+			if (c.isEmpty()) {
+				throw NotFoundException.builder().message("No se encontro la conciliacion para la orden: " + numeroOrden)
+				.build();
+			}
+			return c.get();
+		}
+		 catch (NotFoundException e) {
+				throw NotFoundException.builder().message(e.getMessage()).build();
+			}
+		catch (Exception e) {
+			throw BusinessException.builder().message(e.getMessage()).build();
+		}
+
 	}
 
 }
--- trabajointegrador/src/main/java/ar/iua/edu/trabajointegrador/model/business/interfaces/IConciliacionBusiness.java
+++ trabajointegrador/src/main/java/ar/iua/edu/trabajointegrador/model/business/interfaces/IConciliacionBusiness.java
@@ -1,12 +1,15 @@
 package ar.iua.edu.trabajointegrador.model.business.interfaces;
 
+import java.util.List;
+import java.util.Optional;
+
 import ar.iua.edu.trabajointegrador.model.Conciliacion;
-import ar.iua.edu.trabajointegrador.model.Orden;
 import ar.iua.edu.trabajointegrador.model.business.exceptions.BusinessException;
 import ar.iua.edu.trabajointegrador.model.business.exceptions.NotFoundException;
 import ar.iua.edu.trabajointegrador.model.business.exceptions.StateLoadException;
 
 public interface IConciliacionBusiness {
 	public Conciliacion add(float pesajeFinal, int numeroOrden) throws NotFoundException, BusinessException,StateLoadException;
-
+	public List<Conciliacion> list() throws BusinessException;
+	public Conciliacion loadByNumeroOrden(int numeroOrden)throws BusinessException, NotFoundException;
 }
