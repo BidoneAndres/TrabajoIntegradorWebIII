0d4c385470a7a07da1b6a517f058633fcd8774b3
joaquincernik <joaquincernik@gmail.com>
2026-02-22 13:22:04 -0300

    quite que pida en todas las rutas el auth

--- trabajointegrador/src/main/java/ar/iua/edu/trabajointegrador/security/SecurityConfiguration.java
+++ trabajointegrador/src/main/java/ar/iua/edu/trabajointegrador/security/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,11 +21,21 @@ import ar.iua.edu.trabajointegrador.auth.custom.CustomAuthenticationManager;
 import ar.iua.edu.trabajointegrador.auth.filters.JWTAuthorizationFilter;
 import ar.iua.edu.trabajointegrador.controllers.Constants;
 
-
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled=true)
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		// CORS: https://developer.mozilla.org/es/docs/Web/HTTP/CORS
+		// CSRF:0 https://developer.mozilla.org/es/docs/Glossary/CSRF
+		http.cors(CorsConfigurer::disable);
+		http.csrf(AbstractHttpConfigurer::disable);
+		http.authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll().anyRequest().authenticated());
+		return http.build();
+	}
+
 	@Bean
 	PasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -47,28 +58,28 @@ public class SecurityConfiguration {
 	public AuthenticationManager authenticationManager() {
 		return new CustomAuthenticationManager(bCryptPasswordEncoder(), userBusiness);
 	}
-
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-		
-		/*Basicamente estamos diciendo que no hace falta estar autenticado para autenticarse
-		 *  tambien permitimos las documentaciones y todo eso con los permit all 
-		*/
-		http.csrf(AbstractHttpConfigurer::disable);
-		// Use the global CORS configuration declared in the WebMvcConfigurer
-
-		// Allow access to the login endpoint regardless of HTTP method (tolerant to client mistakes)
-		http.authorizeHttpRequests(auth -> auth.requestMatchers(Constants.URL_LOGIN).permitAll()
-
-				.requestMatchers("/v3/api-docs/**").permitAll().requestMatchers("/swagger-ui.html").permitAll()
-				.requestMatchers("/swagger-ui/**").permitAll().requestMatchers("/ui/**").permitAll() //Todo esto es la documentacion, que nos va a hacer una pagina web
-				.requestMatchers("/demo/**").permitAll().anyRequest().authenticated());
-		//http.httpBasic(Customizer.withDefaults());
-		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-		http.addFilter(new JWTAuthorizationFilter(authenticationManager()));
-		return http.build();
-
-	}
+//
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//		
+//		/*Basicamente estamos diciendo que no hace falta estar autenticado para autenticarse
+//		 *  tambien permitimos las documentaciones y todo eso con los permit all 
+//		*/
+//		http.csrf(AbstractHttpConfigurer::disable);
+//		// Use the global CORS configuration declared in the WebMvcConfigurer
+//
+//		// Allow access to the login endpoint regardless of HTTP method (tolerant to client mistakes)
+//		http.authorizeHttpRequests(auth -> auth.requestMatchers(Constants.URL_LOGIN).permitAll()
+//
+//				.requestMatchers("/v3/api-docs/**").permitAll().requestMatchers("/swagger-ui.html").permitAll()
+//				.requestMatchers("/swagger-ui/**").permitAll().requestMatchers("/ui/**").permitAll() //Todo esto es la documentacion, que nos va a hacer una pagina web
+//				.requestMatchers("/demo/**").permitAll().anyRequest().authenticated());
+//		//http.httpBasic(Customizer.withDefaults());
+//		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//		http.addFilter(new JWTAuthorizationFilter(authenticationManager()));
+//		return http.build();
+//
+//	}
 
 }
