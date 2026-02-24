39e0328295e23a7cad31cd63ad6bb9bbdc64a967
joaquincernik <joaquincernik@gmail.com>
2026-02-24 15:12:05 -0300

    aquivi el myapplication que estaba

--- trabajointegrador/src/main/java/ar/iua/edu/trabajointegrador/BackendApplication.java
+++ trabajointegrador/src/main/java/ar/iua/edu/trabajointegrador/BackendApplication.java
@@ -7,43 +7,29 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import lombok.extern.slf4j.Slf4j;
-
 
 @SpringBootApplication
 @Slf4j
 public class BackendApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
-	@Value("${spring.profiles.active}")
-	private String profile;
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BackendApplication.class);
+    }
 
-	/*@Autowired
-	private IProductCli2Business productCli2Business;
+    @Value("${spring.profiles.active:default}")
+    private String profile;
 
-	@Autowired
-	private ProductRepository productDAO;*/
-
-
-	@Override
-	public void run(String... args) throws Exception {
-		log.info("Perfil Activo '{}'", profile);
-		/*log.info(
-				"Default -------------------------------------------------------------------------------------------------------");
-		productCli2Business.listExpired(new Date());
-		log.info(
-				"Customizada ---------------------------------------------------------------------------------------------------");
-		productCli2Business.listSlim();
-		
-		log.info("Cantidad de productos de la categoría id=3: {}", productDAO.countProductsByCategory(3));
-		log.info("Set stock=true producto id que no existe, resultado={}", productDAO.setStock(true, 333));*/
-
-
-	}
-
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Perfil Activo '{}'", profile);
+    }
 }
